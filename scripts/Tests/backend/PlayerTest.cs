ddbdfcc7752e8b4166fd26be01c6e48fe57d7abc
lawrenceyim <lawrence.yim.dev@gmail.com>
2026-06-21 09:29:00 -0700

    Added unit tests.

---
+++ scripts/Tests/backend/PlayerTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace Tests;
+
+public class PlayerTest {
+    [Fact]
+    public void IncreasePlayerHealthTest() {
+        PlayerRepository repo = new PlayerRepository();
+        PlayerService service = new PlayerService(repo);
+        repo.MaxHealth = 100;
+        repo.Health = 90;
+        int expectedHealth = 100;
+        int expectedAmountIncreased = 10;
+        int amountIncreased = service.IncreaseHealth(20);
+        Assert.Equal(expectedHealth, repo.Health);
+        Assert.Equal(expectedAmountIncreased, amountIncreased);
+    }
+}
